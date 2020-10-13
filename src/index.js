59e18cc08a0da634b94e5134b633f59ed32334ce
Malev0lence <foliant230@gmail.com>
2020-10-13 22:54:28 +0300

    add: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let subStr = str;
+  bracketsConfig = bracketsConfig.flat().join('');
+  for (let j = 0; j < bracketsConfig.length; j+=2) {
+    arr.push(bracketsConfig.slice(j, j+2));
+  }
+  
+  for (let i = 0; i <= str.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      subStr = subStr.replace(arr[j], '');
+    }
+  }
+  
+  return (subStr === "") ? true : false;
 }
